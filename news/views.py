c763b482eb674d42d0d83b5c0a038378868f6a6d
John Smith <smith@mail.com>
2024-07-17 23:50:09 +0300

    create base template base.html

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
 def news(request):
-    return render(request, "news.html")
+    some_news = News.objects.all().order_by('-date')
+    return render(request, "news.html", {'some_news': some_news})
